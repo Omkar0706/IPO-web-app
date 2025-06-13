98ded4bf66298dcb1c6b3c22b33f0330bcb63e75
Omkar Prasad Choudhury <omkarchoudhury0706@gmail.com>
2025-06-13 12:04:21 +0530

    OngoingIPO.jsx

--- frontend/src/IPO/OngoingIPO.jsx
+++ frontend/src/IPO/OngoingIPO.jsx
@@ -9,7 +9,7 @@ const OngoingIPO = () => {
 
   const ongoingIPOs = [
     {
-      name: "New Agritech Ltd.",
+      name: "Nova Agritech Ltd.",
       logo: "",
       priceBand: "₹39 - 41",
       open: "2024-01-22",
@@ -70,8 +70,7 @@ const OngoingIPO = () => {
                       />
                     ) : (
                       <div className="w-12 h-12 bg-gray-200 rounded"></div>
-                    )}                      <h2 className="font-bold text-lg text-[#467CFF]">{ipo.name}</h2>
-                      <img src={ipo.logo} alt={`logo`} className="w-auto h-auto bg-cover bg-center bg-gray-200" />
+                    )}                     
                       <h2 className="font-bold text-lg text-[#467CFF]">{ipo.name}</h2>
                   </div>
                   
